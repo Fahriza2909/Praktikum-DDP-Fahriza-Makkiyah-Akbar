057c971459fa327429e843f205732ced19129131
Fahriza2909 <ryooichikun@gmail.com>
2025-11-26 10:44:19 +0700

    Add files via upload

---
+++ pertemuan 10/bangun datar/panggilan.py
@@ -0,0 +1,17 @@
+import math
+
+def luas_persegi(sisi):
+    return sisi * sisi
+
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+def luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
+
+def luas_lingkaran(jari_jari):
+    return math.pi * jari_jari * jari_jari
+
+def luas_jajar_genjang(alas, tinggi):
+    return alas * tinggi
+
